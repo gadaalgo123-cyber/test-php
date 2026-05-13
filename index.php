9e7c3e926b83ddd26dab7ad7179c61ccf8ec74af
gadaalgo123-cyber <gadaalgo123@gmail.com>
2026-05-13 16:20:38 +0700

    test clone

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 <body>
     <h1>PHP Test</h1>
     <p>This is a simple PHP test page.</p>
-    <?php
+    <?php // pembuka tag php
         echo "Hello, World!";
-        
+    ?> <--penutup tag php -->
+    <?php 
+//adalah sintax php
+    echo "ini adalah <br/> sintax php <br/> "
     ?>
     
 </body>
